36da800a56af9408e78f93387dfbe4bed475334b
Nicolas Turlais <nicolas.turlais@gmail.com>
2017-06-10 20:50:08 +0200

    hello :zap:

---
+++ petit-tonnerre.js
@@ -0,0 +1,46 @@
+var WebSocket = require('ws')
+var { getDistance } = require('geolib')
+
+var myPosition = {
+    latitude: 42.46, // the latitude of my position
+    longitude: 20.63 // the longitude of my position
+}
+var radius = 15000 // the maximum distance around my potion a strike will be detected, (in meters)
+var soundSpeed = 337 // the speed of the sound, (in meters per seconde)
+
+try {
+    var rnd = parseInt(Math.random()*40)+50
+    ws = new WebSocket('ws://ws.blitzortung.org:80' + rnd)
+} catch (e) {
+    console.error(e)
+}
+
+ws.onopen = function() {
+    ws.send('{"west":-180.0,"east":180.0,"north":90.0,"south":-90.0}');
+}
+
+ws.onmessage = function({ data }) {
+    var impact = JSON.parse(data)
+    var impactPosition = {
+        latitude: impact.lat,
+        longitude: impact.lon
+    }
+    var distance = getDistance(myPosition, impactPosition)
+
+    if (Math.floor(distance) > radius) { return }
+
+    var impactIn = distance / soundSpeed // - impact.delay
+    beep(impactIn)
+}
+
+function beep(impactIn) {
+    var remainingSeconds = impactIn
+
+    while(remainingSeconds > 1) {
+        remainingSeconds = remainingSeconds / 2
+        setTimeout(function () {
+            process.stderr.write("\007")
+            console.log('debug: beep')
+        }, (impactIn - remainingSeconds) * 1000)
+    }
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Petit Tonnerre
+
+Detect lighting strikes around you and beeps as the thunder approches :zap:
+
+## Installation
+`git clone https://github.com/nicolas-t/petit-tonnerre.git`  
+`cd petit-tonnerre`.  
+`yarn install`. 
+
+## Configuration
+open `petit-tonnerre.js`,  
+update `myPosition` with your poition's latitude and longitude.
+
+## Usage
+run `node petit-tonnerre.js` from the `petit-tonnerre` folder.  
+install it on a Raspberry Pi and plug a speaker.  
+
+
