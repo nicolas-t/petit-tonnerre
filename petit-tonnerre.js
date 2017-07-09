f83ce1fea1fb264f7454e75edf1459f9ff66123b
Nicolas Turlais <nicolas.turlais@gmail.com>
2017-07-09 14:39:37 +0200

    Use CLI options instead of file editing

--- petit-tonnerre.js
+++ petit-tonnerre.js
@@ -1,15 +1,32 @@
 var WebSocket = require('ws')
 var { getDistance } = require('geolib')
+var argv = require('yargs')
+    .alias('lat', 'latitude')
+    .describe('lat', 'the latitude of your position')
+    .default('lat', 21.3069440, '21.3069440')
 
-var myPosition = {
-    latitude: 42.46, // the latitude of my position
-    longitude: 20.63 // the longitude of my position
-}
-var radius = 15000 // the maximum distance around my position a strike will be detected, (in meters)
-var soundSpeed = 337 // the speed of the sound, (in meters per seconde)
+    .alias('lon', 'longitude')
+    .describe('lon', 'the longitude of your position')
+    .default('lon', -157.8583330, '-157.8583330')
+
+    .alias('r', 'radius')
+    .describe('r', 'the maximum distance around your position a strike will be detected, (in meters)')
+    .default('r', 5000, '5000m')
+
+    .alias('s', 'speed')
+    .describe('s', 'the speed of the sound, (in meters per second)')
+    .default('s', 337, '337m/s')
+
+    .help('help')
+  .argv
+
+var latitude = argv.latitude
+var longitude = argv.longitude
+var radius = argv.radius
+var soundSpeed = argv.speed
 
 try {
-    var rnd = parseInt(Math.random()*40)+50
+    var rnd = parseInt(Math.random() * 40) + 50
     ws = new WebSocket('ws://ws.blitzortung.org:80' + rnd)
 } catch (e) {
     console.error(e)
@@ -25,11 +42,17 @@ ws.onmessage = function({ data }) {
         latitude: impact.lat,
         longitude: impact.lon
     }
+
+    var myPosition = {
+        latitude: latitude,
+        longitude: longitude
+    }
+
     var distance = getDistance(myPosition, impactPosition)
 
     if (Math.floor(distance) > radius) { return }
 
-    var impactIn = distance / soundSpeed // - impact.delay
+    var impactIn = distance / soundSpeed - impact.delay
     beep(impactIn)
 }
 
@@ -40,7 +63,6 @@ function beep(impactIn) {
         remainingSeconds = remainingSeconds / 2
         setTimeout(function () {
             process.stderr.write("\007")
-            console.log('debug: beep')
         }, (impactIn - remainingSeconds) * 1000)
     }
 }
